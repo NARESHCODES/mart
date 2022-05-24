43aed4ca71c08248d2eea68820d329b3811c1523
NARESHCODES <nareshcodes@gmail.com>
2022-05-25 02:00:18 +0545

    carousel slider added

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:carousel_slider/carousel_slider.dart';
 import "package:flutter/material.dart";
+import 'package:get/get.dart';
 import 'package:mart/util/app_color.dart';
 
 class HomeView extends StatelessWidget {
@@ -22,7 +26,36 @@ class HomeView extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(child: Text("Hello world")),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          //slides
+          SizedBox(
+            height: 250,
+            width: Get.size.width,
+            child: CarouselSlider(
+              options: CarouselOptions(height: 400.0),
+              items: [1, 2, 3, 4, 5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(color: Colors.amber),
+                        child: Text(
+                          'text $i',
+                          style: TextStyle(fontSize: 16.0),
+                        ));
+                  },
+                );
+              }).toList(),
+            ),
+          )
+          //featured products
+          //offers
+          //special products
+        ],
+      )),
     ));
   }
 }
